50fbe30357a74486199dc6569fbde8d565973b67
DanyArshith <putla.danyarshith@gmail.com>
2026-07-30 09:52:55 +0530

    Chunking version 1: split the text into 100 characters each and stored it in a list

--- app.py
+++ app.py
@@ -8,4 +8,20 @@ print("no of pages:",total_pages)   # there are 25 page in the document
 
 first_page = reader.pages[0]
 
-print(first_page.extract_text())    # prints the text in the first page
+# print(first_page.extract_text())    # prints the text in the first page
+
+first_page_text = first_page.extract_text()
+
+length = len(first_page_text)
+print(length)
+
+chunks = []
+i = 0
+while i + 100 < length:                     #split the text into 100 characters each chunk
+    chunk = first_page_text[i:i+100]
+    chunks.append(chunk)
+    i += 100
+last_chunk = first_page_text[i:]
+chunks.append(last_chunk)
+
+print(chunks)
