8c2b739636889356047090c17a4513bd3696d8af
DanyArshith <putla.danyarshith@gmail.com>
2026-08-15 13:34:30 +0530

    feat: enhance indexing functionality with save and load capabilities

--- app.py
+++ app.py
@@ -3,28 +3,45 @@ from pathlib import Path
 from preprocess import preprocess_pdf
 from embedder import generate_embeddings
 from model import embedding_model
-from faiss_store import build_index, search
+from faiss_store import (
+    build_index,
+    save_index,
+    load_index,
+    search
+)
 from prompt import build_prompt
 from llm import generate_answer
 
 def main():
     path = Path("data/documents/ML_u1.pdf")
-    chunks = preprocess_pdf(path, chunk_size=300, overlap = 50)
-    chunks = generate_embeddings(chunks)
-    index = build_index(chunks)
 
+    index, chunks = load_index()
+    if index is None or chunks is None:
+        print("No existing index found.")
+        print("Building index...")
+
+        chunks = preprocess_pdf(path, chunk_size=300, overlap = 50)
+        chunks = generate_embeddings(chunks)
+        index = build_index(chunks)
+
+        save_index(index, chunks)
+        print("Index saved.")
+    else:
+        print("Existing index loaded.")
+    
+    
     while True:
         query = input("Ask a question: ").strip()
         if query.lower() == "exit":
             break
 
         query_embedding = embedding_model.encode(query)
-        retrieved_chunks = search(index, query_embedding, chunks, k = 8)
+        retrieved_chunks = search(index, query_embedding, chunks, k = 5)
         prompt = build_prompt(query, retrieved_chunks)
 
-        print("\n\nAnswer:\n")
+        print("\nAnswer:")
         answer = generate_answer(prompt)
-        print()
+        print("\n")
 
 
 if __name__ == "__main__":
--- faiss_store.py
+++ faiss_store.py
@@ -1,5 +1,13 @@
 import faiss
 import numpy as np
+from pathlib import Path
+import pickle
+
+
+STORAGE_DIR = Path("data/storage")
+INDEX_PATH = STORAGE_DIR / "index.faiss"
+CHUNKS_PATH = STORAGE_DIR / "chunks.pkl"
+
 
 def build_index(chunks):
     embeddings = []
@@ -12,6 +20,28 @@ def build_index(chunks):
     index.add(embeddings)
 
     return index
+
+
+def save_index(index, chunks):
+    STORAGE_DIR.mkdir(parents=True, exist_ok=True)
+
+    faiss.write_index(index, str(INDEX_PATH))
+
+    with open(CHUNKS_PATH, "wb") as file:
+        pickle.dump(chunks, file)
+
+
+def load_index():
+    if not INDEX_PATH.exists() or not CHUNKS_PATH.exists():
+        return None, None
+
+    index = faiss.read_index(str(INDEX_PATH))
+
+    with open(CHUNKS_PATH, "rb") as file:
+        chunks = pickle.load(file)
+
+    return index, chunks
+
 
 def search(index, query_embedding, chunks, k):
     query_embedding = query_embedding.reshape(1, -1).astype(np.float32)
